42d2fbf90751f056930eedf274cec647a36e08d1
Julian Barbosa <bjvlixn@gmail.com>
2021-11-23 23:28:41 -0300

    new SearchParams component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1 id="my-brand">Adopt me</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Bean" animal="Dog" breed="Wheaten Terrier" />
+      <SearchParams />
     </div>
   );
 };
