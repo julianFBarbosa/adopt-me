76bbfd2cca8faaeac5c6af0116e911a989957923
Julian Barbosa <bjvlixn@gmail.com>
2021-11-25 21:29:25 -0300

    new SearchParams component

---
+++ src/SearchParams.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+
+  return (
+    <div className="search-params">
+      <form action="">
+        <label htmlFor="location">Location</label>
+        <input
+          type="text"
+          id="location"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+          placeholder="location"
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
