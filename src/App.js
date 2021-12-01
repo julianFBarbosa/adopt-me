d305c9100b94ac561db294629fab81a7443d15a6
Julian Barbosa <bjvlixn@gmail.com>
2021-11-30 21:52:50 -0300

    add details, home, 404 routes and Details component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Details from "./Details";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1 id="my-brand">Adopt me</h1>
-      <SearchParams />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <SearchParams />
+          </Route>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="*">
+            <p>Error 404: Route not found</p>;
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
---
+++ src/Details.js
@@ -0,0 +1,5 @@
+const Details = () => {
+  return <h2>heyeyey</h2>;
+};
+
+export default Details;
