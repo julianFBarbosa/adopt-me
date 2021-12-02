9ef0c461f9e37fcf18b798c1e7a68fbac82a48ae
Julian Barbosa <bjvlixn@gmail.com>
2021-12-02 12:50:03 -0300

    implement Carousel in Details component

--- src/Details.js
+++ src/Details.js
@@ -1,19 +1,16 @@
 import { Component } from "react";
+// eslint-disable-next-line import/named, import/namespace
 import { withRouter } from "react-router";
+import Carousel from "./Carousel";
 
 class Details extends Component {
-  constructor() {
-    super();
-
-    this.state = { loading: true };
-  }
+  state = { loading: true };
 
   async componentDidMount() {
     const res = await fetch(
       `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
     );
     const json = await res.json();
-
     this.setState(
       Object.assign(
         {
@@ -26,12 +23,14 @@ class Details extends Component {
 
   render() {
     if (this.state.loading) {
-      return <h2>loading...</h2>
+      return <h2>loading...</h2>;
     }
-    
-    const { animal, breed, city, state, description, name } = this.state;
+
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
     return (
       <div className="details">
+        <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>
