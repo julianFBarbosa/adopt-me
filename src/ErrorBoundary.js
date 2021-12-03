4c0663e5b595b72527f069db2640816b5f9db094
Julian Barbosa <bjvlixn@gmail.com>
2021-12-02 22:51:55 -0300

    implement autoredirect in ErrorBoundary to homepage

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,24 +1,28 @@
 import { Component } from "react";
 // eslint-disable-next-line import/named, import/namespace
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 
 class ErrorBoundary extends Component {
   state = {
     hasError: false,
   };
+
   static getDerivedStateFromError() {
     return { hasError: true };
   }
 
   componentDidCatch(error, info) {
     console.error("ErrorBoundary caught an error", error, info);
+    setTimeout(() => this.setState({ redirect: true }), 5000);
   }
 
   render() {
-    if (this.state.hasError) {
+    if (this.state.redirect) {
+      return <Redirect to="/" />;
+    } else if (this.state.hasError) {
       return (
         <h2>
-          This Listing has an error{' '}
+          This Listing has an error{" "}
           <Link to="/">Click Here to go back to home page </Link>
         </h2>
       );
